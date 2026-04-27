f13a2458026a890f7bb5bf3f9ec76575f9db2a2b
scotty2hottyy <scottmsonnier@gmail.com>
2026-04-27 11:58:53 -0500

    Delete OOD Files directory
    Deleted old files

--- OOD Files/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Session:
-    """Stores the current state for one connected client."""
-
-    name: str = "Anonymous"
-    room: str | None = None
