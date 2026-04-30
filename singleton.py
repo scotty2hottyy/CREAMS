7b9054a85ac2e51d7a72edd50e087e683e46039c
Empireoverlook-art <Empireoverlook@yahoo.com>
2026-04-30 16:54:00 -0500

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -2,11 +2,8 @@ class SingletonMeta(type):
     """
     Singleton Pattern: a metaclass that ensures only one instance
     of any class using it can ever be created.
-
-    Usage:
-        class MyClass(metaclass=SingletonMeta):
-            ...
     """
+    
     _instances: dict = {}
 
     def __call__(cls, *args, **kwargs):
